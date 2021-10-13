9cf3d61e0778b76cbdb6519c92c6c65b72a7ca14
Kerry Hatcher <kerry.hatcher@protonmail.com>
2021-10-13 16:38:45 -0400

    clear folder if exists already

--- init.sh
+++ init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-
-
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 
 cd
 
+rm -rf jenkins-playbook
+
 git clone https://github.com/kerryhatcher/jenkins-playbook.git
+
+cd jenkins-playbook
 
 export PATH="$HOME/Library/Python/3.8/bin:/opt/homebrew/bin:$PATH"
 
