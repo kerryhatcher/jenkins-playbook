35e1e43c89755bde8368e9edb9f163ae609433f6
Kerry Hatcher <kerry.hatcher@protonmail.com>
2021-10-15 02:12:28 -0400

    Wait for http to be ready

--- tests/testingGround.sh
+++ tests/testingGround.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+#set -e
 
 # docker pull jenkins/jenkins:lts-jdk11
 
@@ -19,17 +19,29 @@ set -e
 # export crumb=$(curl -b cookie.txt -c cookie.txt -v -X GET http://$jenkins_URL/crumbIssuer/api/json --user admin:$(cat ./jenkins_home/secrets/initialAdminPassword) | jq '.crumb')
 
 
-
-export jenkins_URL="localhost:8080"
-
-export crumb=$(curl -b cookie.txt -c cookie.txt -v -X GET http://$jenkins_URL/crumbIssuer/api/json --user admin:$(cat ./jenkins_home/secrets/initialAdminPassword) | jq -r '.crumb')
-
-curl -b cookie.txt -c cookie.txt -X POST -o APItoken.json http://$jenkins_URL/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken --data 'newTokenName=bootstrap' --user admin:$(cat ./jenkins_home/secrets/initialAdminPassword) -H "Jenkins-Crumb: $crumb" 
-
-export jenk_token=$(cat APItoken.json | jq -r '.data.tokenValue')
-
-echo $jenkins_URL
-echo $crumb
-echo $jenk_token
+docker stop jenktest
 
 rm ~/.jenkins-cli.yaml
+
+rm -rf jenkins_hoome/*
+
+sleep 8
+
+docker start jenktest
+
+
+#export jenkins_URL="localhost:8080"
+
+#export crumb=$(curl -b cookie.txt -c cookie.txt -v -X GET http://$jenkins_URL/crumbIssuer/api/json --user admin:$(cat ./jenkins_home/secrets/initialAdminPassword) | jq -r '.crumb')
+
+#curl -b cookie.txt -c cookie.txt -X POST -o APItoken.json http://$jenkins_URL/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken --data 'newTokenName=bootstrap' --user admin:$(cat ./jenkins_home/secrets/initialAdminPassword) -H "Jenkins-Crumb: $crumb" 
+
+#export jenk_token=$(cat APItoken.json | jq -r '.data.tokenValue')
+
+#echo $jenkins_URL
+#echo $crumb
+#echo $jenk_token
+
+#rm APItoken.json
+
+#rm cookie.txt
