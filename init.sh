1c00c8a37f7207d2ae6b6e46b4a0553fc09e58a5
Kerry Hatcher <kerry.hatcher@protonmail.com>
2021-10-18 19:54:02 -0400

    add CLI tools logic and fix playbook commands

--- init.sh
+++ init.sh
@@ -54,7 +54,22 @@ fi
 
 if ask "Install xcode?"; then
     echo "Yes"
-    xcode-select --install
+    echo "Checking Xcode CLI tools"
+    # Only run if the tools are not installed yet
+    # To check that try to print the SDK path
+    xcode-select -p &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Xcode CLI tools not found. Installing them..."
+        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+        PROD=$(softwareupdate -l |
+            grep "\*.*Command Line" |
+            head -n 1 | awk -F"*" '{print $2}' |
+            sed -e 's/^ *//' |
+            tr -d '\n')
+        softwareupdate -i "$PROD" -v;
+    else
+        echo "Xcode CLI tools OK"
+    fi
 
     if ask "Xcode finshed installing?"; then
     echo "Yes"
@@ -124,11 +139,12 @@ if ask "Run playbook?"; then
 
     if [ -d "~/jenkins-playbook" ] 
     then
-        echo "jenkins-playbook" 
+        echo "jenkins-playbook found, updating" 
         cd ~/jenkins-playbook
         git pull
     else
-        echo git clone https://github.com/kerryhatcher/jenkins-playbook.git ~/jenkins-playbook
+        echo "No folder, cloning" 
+        git clone https://github.com/kerryhatcher/jenkins-playbook.git ~/jenkins-playbook
         cd ~/jenkins-playbook
     fi
 
