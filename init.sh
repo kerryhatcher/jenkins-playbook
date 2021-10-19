beab0263e2133938309354bfb8634f7c6d5bc74d
Kerry Hatcher <kerry.hatcher@protonmail.com>
2021-10-18 21:16:38 -0400

    Revert xcode changes

--- init.sh
+++ init.sh
@@ -77,25 +77,26 @@ fi
 
 if ask "Install xcode?"; then
     echo "Yes"
-    echo "Checking Xcode CLI tools"
+    xcode-select --install
+    # echo "Checking Xcode CLI tools"
     # Only run if the tools are not installed yet
     # To check that try to print the SDK path
-    xcode-select -p &> /dev/null
-    if [ $? -ne 0 ]; then
-        echo "Xcode CLI tools not found. Installing them..."
-        touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-        PROD=$(softwareupdate -l |
-            grep "\*.*Command Line" |
-            head -n 1 | awk -F"*" '{print $2}' |
-            sed -e 's/^ *//' |
-            tr -d '\n')
-        softwareupdate -i "$PROD" -v;
+    # xcode-select -p &> /dev/null
+    # if [ $? -ne 0 ]; then
+    #     echo "Xcode CLI tools not found. Installing them..."
+    #     touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+    #     PROD=$(softwareupdate -l |
+    #         grep "\*.*Command Line" |
+    #         head -n 1 | awk -F"*" '{print $2}' |
+    #         sed -e 's/^ *//' |
+    #         tr -d '\n')
+    #     softwareupdate -i "$PROD" -v;
     else
         echo "Xcode CLI tools OK"
     fi
 
     if ask "Xcode finshed installing?"; then
-    echo "Yes"
+        echo "Yes"
     else
         echo "No"
         exit 1
